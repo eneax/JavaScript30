96d04af380eef17d7a29e4cf0344bded485858df
eneax <19818937+eneax@users.noreply.github.com>
2018-06-23 17:28:07 +0200

    Initial commit project 7

---
+++ 07 - Array Cardio Day 2/README.md
@@ -0,0 +1,3 @@
+# #JavaScript30 Day 7: Array Cardio Day 2
+
+Day 7 of #JavaScript30 and another array cardio day!
---
+++ 07 - Array Cardio Day 2/script.js
@@ -0,0 +1,54 @@
+const people = [
+  { name: 'Wes', year: 1988 },
+  { name: 'Kait', year: 1986 },
+  { name: 'Irv', year: 1970 },
+  { name: 'Lux', year: 2015 }
+];
+
+const comments = [
+  { text: 'Love this!', id: 523423 },
+  { text: 'Super good', id: 823423 },
+  { text: 'You are the best', id: 2039842 },
+  { text: 'Ramen is my fav food ever', id: 123523 },
+  { text: 'Nice Nice Nice!', id: 542328 }
+];
+
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+// const isAdult = people.some(function(person) {
+//   const currentYear = (new Date()).getFullYear();
+//   if (currentYear - person.year >= 19) {
+//     return true
+//   }
+// });
+
+// const isAdult = people.some((person) => {
+//   const currentYear = (new Date()).getFullYear();
+//   return currentYear - person.year >= 19;
+// });
+
+const isAdult = people.some((person) => ((new Date()).getFullYear()) - person.year >= 19);
+console.log({isAdult});
+
+// Array.prototype.every() // is everyone 19 or older?
+const allAdults = people.every((person) => (new Date().getFullYear()) - person.year >= 19);
+console.log({allAdults});
+
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for --> it's going to return you the first item that it finds
+// find the comment with the ID of 823423
+const comment = comments.find((comment) => comment.id === 823423);
+console.log(comment);
+
+// Array.prototype.findIndex()
+// Find the comment with this ID
+const index = comments.findIndex((comment) => comment.id === 823423);
+console.log(index); // gives you 1 --> the second item in the array
+
+// delete the comment with the ID of 823423
+comments.splice(index, 1);
+// OR create a new array without the deleted ID
+const newComments = [
+  ...comments.slice(0, index),
+  ...comments.slice(index + 1)
+];
