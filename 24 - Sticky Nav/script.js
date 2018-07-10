5381690b0f1dfc96e3aa2f2ac580fa0a3b7ba376
eneax <19818937+eneax@users.noreply.github.com>
2018-07-10 11:59:33 +0200

    Add slide-in animations in project 24

--- 24 - Sticky Nav/script.js
+++ 24 - Sticky Nav/script.js
@@ -12,3 +12,42 @@ function fixNav() {
 }
 
 window.addEventListener('scroll', fixNav);
+
+
+function debounce(func, wait = 20, immediate = true) {
+  var timeout;
+  return function() {
+    var context = this, args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+// runs every time a person scrolls
+function checkSlide() {
+  sliderImages.forEach((sliderImage) => {
+    // half way through the image
+    const slideInAt = (window.scrollY + window.innerHeight) - (sliderImage.height / 2);
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.screenY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add('active');
+    } else {
+      sliderImage.classList.remove('active');
+    }
+  })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
